ababa85b616ba5b34d5c30105b9d3007775dafa9
helpfulrobot <chris@silverstripe.com>
2016-01-01 04:54:26 +1300

    Converted to PSR-2

--- code/KeyboardShortcuts.php
+++ code/KeyboardShortcuts.php
@@ -1,9 +1,10 @@
 <?php
-class KeyboardShortcuts extends Extension {
-    public function onAfterInit() {
+class KeyboardShortcuts extends Extension
+{
+    public function onAfterInit()
+    {
         Requirements::javascript(KEYBOARD_SHORTCUTS_BASE.'/javascript/external/mousetrap/mousetrap.min.js');
         Requirements::javascript(KEYBOARD_SHORTCUTS_BASE.'/javascript/external/mousetrap/plugins/mousetrap-global-bind.min.js');
         Requirements::javascript(KEYBOARD_SHORTCUTS_BASE.'/javascript/KeyboardShortcuts.js');
     }
 }
-?>
